60664a6c0414328788e02d9542275b2d006bdcc6
AsylkhanTurganbekov <210103295@stu.sdu.edu.kz>
2023-06-30 16:55:45 +0600

    Second Commit.

--- guru.springframework/src/main/java/com/example/sfgpetclinic/Application.java
+++ guru.springframework/src/main/java/com/example/sfgpetclinic/Application.java
@@ -2,12 +2,19 @@ package com.example.sfgpetclinic;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class Application {
 
     public static void main(String[] args) {
+//       ConfigurableApplicationContext ctx =   SpringApplication.run(Application.class, args); // DP Injection without Spring
+//       MyController myController = (MyController) ctx.getBean("myController");
+//
+//
+//       String greeting = myController.sayHello();
+//        System.out.println(greeting);
+
         SpringApplication.run(Application.class, args);
     }
-
 }
