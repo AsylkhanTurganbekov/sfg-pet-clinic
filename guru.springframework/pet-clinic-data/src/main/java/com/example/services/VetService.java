f2e19dd6f4760810f013844f0c266e10215540d0
AsylkhanTurganbekov <210103295@stu.sdu.edu.kz>
2023-07-01 10:03:45 +0600

    Implement Interface for service .closes #5

---
+++ guru.springframework/pet-clinic-data/src/main/java/com/example/services/VetService.java
@@ -0,0 +1,12 @@
+package com.example.services;
+
+import com.example.model.Pet;
+import com.example.model.Vet;
+
+import java.util.Set;
+
+public interface VetService {
+    Vet findById(Long id);
+    Vet save(Vet vet);
+    Set<Vet> findAll();
+}
