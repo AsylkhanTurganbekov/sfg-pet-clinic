55cd8073694e2a855f91a7011d91d2c65f996420
AsylkhanTurganbekov <210103295@stu.sdu.edu.kz>
2023-07-04 12:43:35 +0600

    refactor Map imp to abstract. closes #23.

--- guru.springframework/pet-clinic-web/src/main/java/com/example/boostrap/DataLoader.java
+++ guru.springframework/pet-clinic-web/src/main/java/com/example/boostrap/DataLoader.java
@@ -24,14 +24,12 @@ public class DataLoader implements CommandLineRunner {
     @Override
     public void run(String... args) throws Exception {
         Owner owner1 = new Owner();
-        owner1.setId(1L);
         owner1.setFirstName("Michael");
         owner1.setLasNamel("Weston");
 
         ownerService.save(owner1);
 
         Owner owner2 = new Owner();
-        owner2.setId(2L);
         owner2.setFirstName("Fionna");
         owner2.setLasNamel("Glenne");
 
@@ -40,14 +38,12 @@ public class DataLoader implements CommandLineRunner {
         System.out.println("Loaded Owners..");
 
         Vet vet1 = new Vet();
-        vet1.setId(1L);
         vet1.setFirstName("Lastim");
         vet1.setLasNamel("Akamaru");
 
         vetService.save(vet1);
 
         Vet vet2 = new Vet();
-        vet2.setId(2L);
         vet2.setFirstName("Kessie");
         vet2.setLasNamel("Porter");
 
