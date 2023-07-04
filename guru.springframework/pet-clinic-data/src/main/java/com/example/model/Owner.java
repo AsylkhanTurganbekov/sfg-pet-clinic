9e2ec8ec17404de2680377b96e0c0a09ffbcc363
AsylkhanTurganbekov <210103295@stu.sdu.edu.kz>
2023-07-04 19:36:23 +0600

    Added Owner services and etc. closes #28.

--- guru.springframework/pet-clinic-data/src/main/java/com/example/model/Owner.java
+++ guru.springframework/pet-clinic-data/src/main/java/com/example/model/Owner.java
@@ -3,13 +3,52 @@ package com.example.model;
 import java.util.Set;
 
 public class Owner extends Person { // For Pojo1
+
+    private String address;
+    private Set<Pet>  pet;
+    private String city;
+    private String telephone;
+    private Set<Owner> pets;
+
     public Set<Owner> getPets() {
         return pets;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public Set<Pet> getPet() {
+        return pet;
+    }
+
+    public void setPet(Set<Pet> pet) {
+        this.pet = pet;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getTelephone() {
+        return telephone;
+    }
+
+    public void setTelephone(String telephone) {
+        this.telephone = telephone;
     }
 
     public void setPets(Set<Owner> pets) {
         this.pets = pets;
     }
 
-    private Set<Owner> pets;
+
 }
