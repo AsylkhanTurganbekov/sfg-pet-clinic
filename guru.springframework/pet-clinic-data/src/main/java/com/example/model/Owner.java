4e4674bd5b3318adcd1b7941a7fdd56385b991c1
AsylkhanTurganbekov <210103295@stu.sdu.edu.kz>
2023-07-04 19:28:44 +0600

    Added PetType,Visit. closes #26.

--- guru.springframework/pet-clinic-data/src/main/java/com/example/model/Owner.java
+++ guru.springframework/pet-clinic-data/src/main/java/com/example/model/Owner.java
@@ -1,4 +1,15 @@
 package com.example.model;
 
+import java.util.Set;
+
 public class Owner extends Person { // For Pojo1
+    public Set<Owner> getPets() {
+        return pets;
+    }
+
+    public void setPets(Set<Owner> pets) {
+        this.pets = pets;
+    }
+
+    private Set<Owner> pets;
 }
--- guru.springframework/pet-clinic-data/src/main/java/com/example/model/Visit.java
+++ guru.springframework/pet-clinic-data/src/main/java/com/example/model/Visit.java
@@ -1,4 +1,33 @@
 package com.example.model;
 
-public class Visit { // For Pojo1
+import java.time.LocalDate;
+
+public class Visit extends BaseEntity { // For Pojo1
+    private LocalDate date;
+    private String description;
+    private Pet pet;
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public void setDate(LocalDate date) {
+        this.date = date;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Pet getPet() {
+        return pet;
+    }
+
+    public void setPet(Pet pet) {
+        this.pet = pet;
+    }
 }
