3f77e73b684d9a6c1bbff98a88387ab26081c5b2
AsylkhanTurganbekov <210103295@stu.sdu.edu.kz>
2023-07-04 19:32:44 +0600

    Added Speciality, Vet Associate. closes #27.

--- guru.springframework/pet-clinic-data/src/main/java/com/example/model/Speciality.java
+++ guru.springframework/pet-clinic-data/src/main/java/com/example/model/Speciality.java
@@ -1,4 +1,13 @@
 package com.example.model;
 
-public class Speciality { // For Pojo1
+public class Speciality extends BaseEntity { // For Pojo1
+    private String description;
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
 }
--- guru.springframework/pet-clinic-data/src/main/java/com/example/model/Vet.java
+++ guru.springframework/pet-clinic-data/src/main/java/com/example/model/Vet.java
@@ -1,4 +1,15 @@
 package com.example.model;
 
+import java.util.Set;
+
 public class Vet  extends Person { // For Pojo1
+    public Set<Speciality> getSpecialitySet() {
+        return specialitySet;
+    }
+
+    public void setSpecialitySet(Set<Speciality> specialitySet) {
+        this.specialitySet = specialitySet;
+    }
+
+    private Set<Speciality> specialitySet;
 }
