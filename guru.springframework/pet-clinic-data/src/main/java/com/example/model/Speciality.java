b130e94722e88338405b8e4e9a4d65c00965433c
AsylkhanTurganbekov <210103295@stu.sdu.edu.kz>
2023-07-05 16:10:39 +0600

    Updated Vet with JPA and Entities. closes #37.

--- guru.springframework/pet-clinic-data/src/main/java/com/example/model/Speciality.java
+++ guru.springframework/pet-clinic-data/src/main/java/com/example/model/Speciality.java
@@ -1,6 +1,13 @@
 package com.example.model;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "aspecialties")
 public class Speciality extends BaseEntity { // For Pojo1
+    @Column(name = "description")
     private String description;
 
     public String getDescription() {
--- guru.springframework/pet-clinic-data/src/main/java/com/example/model/Vet.java
+++ guru.springframework/pet-clinic-data/src/main/java/com/example/model/Vet.java
@@ -1,9 +1,18 @@
 package com.example.model;
 
+import jakarta.persistence.*;
+
 import java.util.HashSet;
 import java.util.Set;
 
+@Entity
+@Table(name = "vets")
 public class  Vet  extends Person { // For Pojo1
+
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinTable(name = "vet_specialties",
+            joinColumns = @JoinColumn(name= "vet_id"),
+            inverseJoinColumns = @JoinColumn(name = "speciality_id"))
     public Set<Speciality> getSpecialitySet() {
         return specialitySet;
     }
