c1fbd6f88f0cdaa845d5117f3f7991db713c118e
AsylkhanTurganbekov <210103295@stu.sdu.edu.kz>
2023-07-06 19:20:31 +0600

    Implemedted Map Visit Service . closes #46.

---
+++ guru.springframework/pet-clinic-data/src/main/java/com/example/services/map/VisitMapService.java
@@ -0,0 +1,39 @@
+package com.example.services.map;
+
+import com.example.model.Visit;
+import com.example.repositories.VisitRepository;
+import com.example.services.VisitService;
+import org.springframework.stereotype.Service;
+
+import java.util.Set;
+@Service
+public class VisitMapService extends AbstractMapService<Visit,Long> implements VisitService {
+
+    @Override
+    public Set<Visit> findall() {
+        return super.findAll();
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+
+    @Override
+    public void delete(Visit object) {
+        super.delete(object);
+    }
+
+    @Override
+    public Visit save(Visit visit) {
+        if(visit.getPet() == null  || visit.getPet().getOwner() == null || visit.getPet().getId()==null || visit.getPet().getOwner().getId() == null) {
+            throw new RuntimeException("Invalid visit");
+        }
+        return super.save(visit);
+    }
+
+    @Override
+    public Visit findById(Long id) {
+        return super.findById(id);
+    }
+}
