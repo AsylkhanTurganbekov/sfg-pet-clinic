0d409585cfaf80312209a681f050322be6354c75
AsylkhanTurganbekov <210103295@stu.sdu.edu.kz>
2023-07-06 17:50:08 +0600

    Create JPA service for PetType. closes #43.

--- guru.springframework/pet-clinic-data/src/main/java/com/example/services/springdatajpa/PetTypeSDJpaService.java
+++ guru.springframework/pet-clinic-data/src/main/java/com/example/services/springdatajpa/PetTypeSDJpaService.java
@@ -4,10 +4,14 @@ import com.example.model.Owner;
 import com.example.model.PetType;
 import com.example.repositories.PetTypeRepository;
 import com.example.services.PetTypeService;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
 
 import java.util.HashSet;
 import java.util.Set;
 
+@Service
+@Profile("springdatajpa")
 public class PetTypeSDJpaService implements PetTypeService {
     private final PetTypeRepository petTypeRepository;
 
